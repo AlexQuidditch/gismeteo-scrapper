067b6bc38c628ebca2181d8aacd021c13b0d8754
AlexQuidditch <alexquidditch@gmail.com>
2021-01-06 16:28:55 +0300

    fix types

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,22 +1,22 @@
 import cheerio from 'cheerio'
 
-export function parseDaysFrom(selector: Cheerio) {
+export function parseDaysFrom(selector: cheerio.Cheerio) {
   const days: string[] = []
   selector.each((i, el) => days.push(cheerio(el).text()))
   return days
 }
 
-export function parseTextFrom(selector: Cheerio) {
+export function parseTextFrom(selector: cheerio.Cheerio) {
   return selector.text()
 }
 
-export function parseMorningWindFrom(selector: Cheerio) {
+export function parseMorningWindFrom(selector: cheerio.Cheerio) {
   const values: string[] = []
   selector.each((i, el) => values.push(cheerio(el).text().replace(' ', ',')))
   return values
 }
 
-export function parseEveningWindFrom(selector: Cheerio) {
+export function parseEveningWindFrom(selector: cheerio.Cheerio) {
   const values: string[] = []
   selector.each((i, el) => values.push(cheerio(el).text().replace(' ', ',')))
   return values
